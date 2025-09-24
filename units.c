d55715e13fd6f62c0f86cbfcedfb8409d07182e5
shariflukalia514-maker <shariflukalia514@gmail.com>
2025-09-24 17:25:04 +0300

    Add files via upload

---
+++ units.c
@@ -0,0 +1,39 @@
+//Simple program displaying all units 
+
+/*
+Name:Shariff Lukalia 
+Reg no: PA106/G/28761/25
+Description simple program displaying all units for this semester 
+*/
+
+#include<stdio.h>
+
+//main function 
+int main(){
+      
+    printf("Discrete Mathematics\n");
+    printf("Mathematics for Science\n");
+    printf("Fundamentals of ICT\n");
+    printf("Computer system and organisation\n");
+    printf("Communication skills\n");
+    printf("Introduction to programming and problem solving\n");
+    printf("Fundamentals of system analysis\n");
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+    
+    
+    
