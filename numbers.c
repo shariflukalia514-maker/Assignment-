dab56251dcf9822770a8c107e235e989492935bd
shariflukalia514-maker <shariflukalia514@gmail.com>
2025-10-09 18:29:38 +0300

    Add files via upload

---
+++ numbers.c
@@ -0,0 +1,18 @@
+/* 
+Name:Shariff lukalia
+Reg no:PA106/G/28761/25
+Date:06/10/2025
+Description :Program displaying numbers 100 to 50 in descending order
+
+*/
+#include<stdio.h>//preprocessor directive 
+// main function 
+int main(){
+    int i;
+    for( i=100; i>=50;i--) {
+    printf("%d\n",i);
+    }
+
+return 0;
+
+}
